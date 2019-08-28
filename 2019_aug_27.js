3bac1b0496db48396b9e3a8632d8867f5ef9ac36
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-27 21:47:58 -0400

    completed an exercise

--- 2019_aug_27.js
+++ 2019_aug_27.js
@@ -8,3 +8,17 @@ for(let count=0; count<7; count++) {
   str += '#';
 }
 str = ''
+
+
+
+
+// fizzbuzz
+for(let count=1; count<101; count++) {
+  if (count % 3 == 0 && count % 5 == 0) {
+    console.log('FizzBuzz')
+  } else if (count % 3 == 0){
+    console.log('Fizz')
+  } else if (count % 5) {
+    console.log('Buzz')
+  }
+}
