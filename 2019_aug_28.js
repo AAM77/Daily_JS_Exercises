1e9a00acc5232c2d8da4c7bd28663a0963889931
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-08-28 11:11:12 -0400

    completed a new exercise.

--- 2019_aug_28.js
+++ 2019_aug_28.js
@@ -9,8 +9,37 @@ let str = '';
 
 let count = 1;
 while (count < 9) {
-  str += '# # # #\n';
+  str += '# # # # \n';
   count++
 }
 
 console.log(str);
+
+// Create a customizable Chessboard / grid
+
+function create_grid(height, width) {
+
+  if (height) {
+    if (width) {
+      let str = '';
+      let count = 1;
+      let maxCount = height+1;
+      while (count < maxCount) {
+        for (let i=0; i < width; i++) {
+          if (i != width-1) {
+            str += '# ';
+          } else {
+            str += '#\n';
+          }
+        }
+        count++
+      }
+
+      console.log(str);
+    } else {
+      console.log("Error: Must have width!");
+    }
+  } else {
+    console.log("Error: Must have height!");
+  }
+}
